bd1e7fcb9ee1e2b1dc290e07cec498fb7d2bd2bc
Pyaeh Phyoe Ko Ko <87226884+pyaeh-phyoe@users.noreply.github.com>
2023-02-11 19:59:57 +0630

    Update useSignup.tsx

--- frontend/src/hooks/useSignup.tsx
+++ frontend/src/hooks/useSignup.tsx
@@ -16,7 +16,7 @@ export const useSignup = () => {
       body: JSON.stringify({ email, password })
     })
     const json = await response.json()
-
+    console.log('json')
     console.log(json)
 
     if (!response.ok) {
